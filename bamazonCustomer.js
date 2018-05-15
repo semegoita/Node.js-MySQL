9da04c79ad4e74579fd2a7e64c706762f774a8f3
semegoita <semere5ha@yahoo.com>
2018-05-15 15:42:09 -0700

    node.js some logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,29 @@
 var sql = require ("mysql");
-var 
+var inquirer = require ("inquierr");
+
+var connection = mysql.createConnection({
+    host:"local host",
+    port:3306,
+    user:"",
+    password:"",
+    database:"bamazon_DB"
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+  function start(){
+      inquirer.prompt([
+          {
+          name:"item",
+          type:"input",
+          message:"what is the ID of the product you are looking for?"
+          },
+          {
+            name:"item",
+            type:"input",
+            message:"How many units of that item are you looking for?",
+           
+          }
+      ]);
+  };
