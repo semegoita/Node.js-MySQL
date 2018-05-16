f660a6823c6dcdfff89776b92d39cd5672a9fd47
semegoita <semere5ha@yahoo.com>
2018-05-16 00:20:56 -0700

    inquirer added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,49 @@
-var mysql = require ("mysql");
-var inquirer = require ("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     port: 3306,
-    user:"root",
-    password:"",
-    database:"bamazon_DB"
+    user: "root",
+    password: "",
+    database: "bamazon_DB"
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected");
-  });
+    runSearch();
+});
+function runSearch() {
+    inquirer
+        .prompt([
+            {
+            name: "productID",
+            type: "input",
+            message: "What is the product_ID of the item you are interested?"
+            },
+            {
+            name: "amount",
+            type: "input",
+            message: "How many of this product are you interested in buying?",
+            }
+        ])
+        .then(function (answer) {
+            // switch (answer.action) {
+            //     case "Find songs by artist":
+            //         artistSearch();
+            //         break;
+
+            //     case "Find all artists who appear more than once":
+            //         multiSearch();
+            //         break;
+
+            //     case "Find data within a specific range":
+            //         rangeSearch();
+            //         break;
+
+            //     case "Search for a specific song":
+            //         songSearch();
+            //         break;
+            // }
+        });
+}
